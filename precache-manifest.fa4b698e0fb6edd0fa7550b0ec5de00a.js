32fe0ae538a125143618f98dc7b88a0cb20b640d
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2020-09-11 23:04:44 -0400

    Updates

---
+++ precache-manifest.fa4b698e0fb6edd0fa7550b0ec5de00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad4d52f5d348a06d586ef1ca5d4e7eb",
+    "url": "/react-nyc-adventure/index.html"
+  },
+  {
+    "revision": "1f845a4fc3df2e104607",
+    "url": "/react-nyc-adventure/static/css/main.94a7f3bc.chunk.css"
+  },
+  {
+    "revision": "9581981ce1770e3caf06",
+    "url": "/react-nyc-adventure/static/js/2.001d2a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-nyc-adventure/static/js/2.001d2a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f845a4fc3df2e104607",
+    "url": "/react-nyc-adventure/static/js/main.6545f356.chunk.js"
+  },
+  {
+    "revision": "5e09df2e50844a6eb371",
+    "url": "/react-nyc-adventure/static/js/runtime-main.f2881c51.js"
+  }
+]);
